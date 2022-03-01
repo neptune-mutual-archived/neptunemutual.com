56cc48d6d895427b1eef88226e6a8e4e2e4f5baa
CarloCarpio <95846026+carlo-neptune@users.noreply.github.com>
2022-03-01 19:48:11 +0800

    Twitter Card Image
    Make the og image url to a be a dynamic value.

--- components/utils/SEO/index.jsx
+++ components/utils/SEO/index.jsx
@@ -1,39 +1,50 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export const SEOMeta = ({
   title,
   description,
   canonical,
   iconHref = "/favicon.ico",
-}) => (
-  <Head>
-    <title>{title}</title>
+}) => {
+  const router = useRouter();
+  const [host, setHost] = useState("");
 
-    <meta name="description" content={description} />
-    <meta name="og:title" property="og:title" content={title} />
-    <meta
-      name="og:description"
-      property="og:description"
-      content={description}
-    />
-    <meta property="og:url" content={canonical} />
+  useEffect(() => {
+    const host = window.location.host;
+    const baseUrl = `https://${host}`;
 
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
+    setHost(baseUrl);
+  }, [router.pathname]);
 
-    <link rel="icon" href={iconHref} />
-    <link rel="apple-touch-icon" href={iconHref} />
+  return (
+    <Head>
+      <title>{title}</title>
 
-    <meta property="og:type" content="website" />
-    <meta property="og:image" content="/images/og.png" />
-    <meta
-      name="twitter:image"
-      content="https://neptunemutual.com/images/og.png"
-    />
+      <meta name="description" content={description} />
+      <meta name="og:title" property="og:title" content={title} />
+      <meta
+        name="og:description"
+        property="og:description"
+        content={description}
+      />
+      <meta property="og:url" content={canonical} />
 
-    <link rel="canonical" href={canonical} />
-  </Head>
-);
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+
+      <link rel="icon" href={iconHref} />
+      <link rel="apple-touch-icon" href={iconHref} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/images/og.png" />
+      <meta name="twitter:image" content={`${host}/images/og.png`} />
+
+      <link rel="canonical" href={canonical} />
+    </Head>
+  );
+};
 
 export default SEOMeta;
